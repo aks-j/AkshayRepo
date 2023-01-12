7d1733ca581d3823939f069b5a025300af012609
aks-j <122248366+aks-j@users.noreply.github.com>
2023-01-12 15:25:07 +0530

    Create $$readonly.sh

---
+++ $$readonly.sh
@@ -0,0 +1,11 @@
+NAME=Akshay
+readonly NAME
+NAME=Anand
+echo "my name is: $NAME"
+echo '-----------------------------------------------------------'
+echo "script name: $0"
+echo "1st cmdls: $1"
+echo "2nd cmdla: $2"
+echo "cmdla list: $@"
+echo "no of cmdl: $#"
+echo '----------------------------------------------------------'
