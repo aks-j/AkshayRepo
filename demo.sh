98cff41692d5af822123cc735b7f0d7fbac77a14
aks-j <122248366+aks-j@users.noreply.github.com>
2023-01-12 15:11:03 +0530

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This script is to read input from the user/console
+echo "Value of a"
+read a
+echo "Value of b"
+read b
+echo "Hello value of a and b is $a $b"
+echo "This is sample change"
+echo "bye"
+echo "hi"
